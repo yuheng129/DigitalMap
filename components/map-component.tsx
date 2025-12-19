02a22426a0a33624ec165f9ed415de1f580bbdec
yuheng129 <toehyuheng@gmail.com>
2025-12-19 15:23:10 +0800

    map fixed

--- components/map-component.tsx
+++ components/map-component.tsx
@@ -10,18 +10,18 @@ import { useEffect } from "react"
 const createCustomIcon = (isVisited: boolean) => {
   // Passport colors: Accent (#c4a57b) for unvisited, Footer/Dark (#3a2515) for visited
   const color = isVisited ? "#3a2515" : "#c4a57b"
-  
+
   const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${color}" stroke="${isVisited ? '#f5ead6' : '#3a2515'}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
       <path d="M12 2c-4.418 0-8 3.582-8 8 0 5.25 8 12 8 12s8-6.75 8-12c0-4.418-3.582-8-8-8z" />
       <circle cx="12" cy="10" r="3" fill="${isVisited ? '#f5ead6' : '#fff9e6'}" stroke="none" />
     </svg>
   `
-  
+
   return L.divIcon({
     className: "custom-map-marker",
     html: svg,
-    iconSize: [48, 63], 
+    iconSize: [48, 63],
     iconAnchor: [24, 63],
     popupAnchor: [0, -66],
   })
@@ -51,11 +51,11 @@ export default function MapComponent({ venues, userVisits, onVenueClick, onMapCl
 
   // Center on Kuala Lumpur roughly
   const centerPosition: [number, number] = [3.15, 101.695]
-  
-  // Malaysia Bounds
+
+  // Tight bounds around the center - limits dragging to ~5km radius
   const maxBounds: L.LatLngBoundsExpression = [
-    [0.0, 95.0],
-    [8.0, 120.0],
+    [3.10, 101.645],  // Southwest corner
+    [3.20, 101.745],  // Northeast corner
   ]
 
   return (
@@ -90,20 +90,22 @@ export default function MapComponent({ venues, userVisits, onVenueClick, onMapCl
           margin: 10px 14px;
         }
       `}</style>
-      
-      <MapContainer 
-        center={centerPosition} 
-        zoom={14} 
+
+      <MapContainer
+        center={centerPosition}
+        zoom={14}
         style={{ height: "100%", width: "100%", zIndex: 0 }}
-        minZoom={10}
+        minZoom={12}
+        maxZoom={16}
         maxBounds={maxBounds}
-        attributionControl={false} // Removed as requested consistently
+        maxBoundsViscosity={1.0}
+        attributionControl={false}
       >
         <TileLayer
           // Use CartoDB Light but tweaked with CSS filter above
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
-        
+
         <MapEvents onMapClick={onMapClick} />
 
         {venues.map((venue) => {
